7a39b2beb35f89d37b85bc9023bf0e6f4adb1d98
Vladislav Veluga <vladislav805@yandex.com>
2018-02-02 16:04:43 +0300

    Rename all constants of settings

--- app/src/main/java/ru/vlad805/timelapse/Setting.java
+++ app/src/main/java/ru/vlad805/timelapse/Setting.java
@@ -3,22 +3,26 @@ package ru.vlad805.timelapse;
 abstract public class Setting {
 	public static final String NAME = "TimeLapse";
 
-	public static final String INTRO = "INTRO";
+	public static final String INTRO = "intro";
 
-	public static final String EFFECT = "COLOR_EFFECT";
-	public static final String DELAY = "DELAY";
-	public static final String FPS = "FPS";
-	public static final String HEIGHT = "HEIGHT";
-	public static final String INTERVAL = "INTERVAL";
-	public static final String QUALITY = "QUALITY";
-	public static final String WHITE_BALANCE = "WHITE_BALANCE";
-	public static final String WIDTH = "WIDTH";
-	public static final String ZOOM = "ZOOM";
-	public static final String WORK_DIRECTORY = "PATH";
-	public static final String FLASH_MODE = "FLASH_MODE";
-	public static final String RECORD_MODE = "RECORD_MODE";
-	public static final String HANDLER = "HANDLER";
-	public static final String REMOTE_CONTROL = "REMOTE_CONTROL";
+	public static final String EFFECT = "colorEffect";
+	public static final String WHITE_BALANCE = "whiteBalance";
+	public static final String DELAY = "delay";
+	public static final String INTERVAL = "interval";
+	public static final String QUALITY = "quality";
+	public static final String FLASH_MODE = "flashMode";
+
+	public static final String FPS = "fps";
+	public static final String WIDTH = "width";
+	public static final String HEIGHT = "height";
+	public static final String WORK_DIRECTORY = "workDirectoryPath";
+
+	public static final String ZOOM = "zoom";
+
+	public static final String RECORD_MODE = "recordMode";
+	public static final String HANDLER = "imageHandler";
+	public static final String REMOTE_CONTROL = "remoteControl";
+
 	public static final String SIZE = "size";
 
 	public static final int INTRO_ABOUT_PLAYING = 0x1;
